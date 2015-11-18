0f45ce8ce2ddee1e487b0740dfedbb1f30d089d6
k4zzk <kosuke19952000@gmail.com>
2015-11-18 21:21:54 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+var remote = require('remote');
+var fs = remote.require('fs');
+var exec = remote.require('child_process').exec;
+
+var ECT = remote.require('ect');
+
+var $ = require("./lib/js/jquery-2.1.4.min.js");
+
+$("#nginx_start").on("click", function(){
+  exec('cp ./template/* ./cache/', function(err, stdout, stderr){
+    console.log("Copy")
+  })
+
+  var renderer = ECT({ root : __dirname + '/template' });
+  var data = { pwd: 'Hello, World!' };
+  var out = renderer.render('nginx.conf.ect', data);
+  fs.writeFile('./cache/nginx.conf', out , function (err) {
+    if (err) { throw err; }
+        console.log ('Save');
+  });
+
+  exec('nginx -p `pwd` -c ./cache/nginx.conf', function(err, stdout, stderr){
+    console.log('server started!');
+  });
+
+  console.log("nginx_start");
+});
+$("#nginx_stop").on("click", function(){
+  exec('nginx -s stop', function(err, stdout, stderr){
+    console.log('server stoped!');
+  });
+});
