521ab0e1f3048c38e2bc3e500279aad4b57b7758
k4zzk <kosuke19952000@gmail.com>
2015-12-15 02:20:31 +0900

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
-var gulp    = require("gulp");
-var install = require("gulp-install");
+var gulp    = require("gulp"),
+    install = require("gulp-install"),
+    sass    = require('gulp-sass'),
+    cssnext = require('gulp-cssnext');
 
 gulp.task('install', function(){
   gulp.src(['./package.json', './src/package.json'])
   .pipe(install());
 });
 
-gulp.task('default', ['install']);
+gulp.task('sass', function() {
+  return gulp.src('src/style/**/*.scss')
+    .pipe(sass())
+    .on('error', function(err) {
+      console.log(err.message);
+    })
+    .pipe(cssnext())
+    .pipe(gulp.dest('src/style/'))
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('./src/style/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
